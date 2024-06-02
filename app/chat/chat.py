daf1c3065b3a57829060d95d4e934ac6b915f082
Mohammed Farid <dev.mohammedfarid@gmail.com>
2024-06-02 13:09:03 +0300

    pdf processing project

--- app/chat/chat.py
+++ app/chat/chat.py
@@ -1,16 +1,56 @@
+import random
+from app.chat.chains.retrieval import StreamingConversationalRetrievalChain
 from app.chat.models import ChatArgs
+from app.chat.vector_stores import retriever_map
+from app.chat.llms import llm_map
+from app.chat.memories import memory_map
+from langchain.chat_models import ChatOpenAI
+from app.web.api import (
+    set_conversation_components,
+    get_conversation_components
+)
+from app.chat.score import random_component_by_score
 
+
+def select_component(component_type, component_map, chat_args: ChatArgs):
+    components = get_conversation_components(chat_args.conversation_id)
+
+    previous_component = components[component_type]
+
+    if previous_component:
+        builder = component_map[previous_component]
+        return previous_component, builder(chat_args)
+
+    random_name = random_component_by_score(
+        component_type,
+        component_map
+    )
+    builder = component_map[random_name]
+    return random_name, builder(chat_args)
 
 def build_chat(chat_args: ChatArgs):
-    """
-    :param chat_args: ChatArgs object containing
-        conversation_id, pdf_id, metadata, and streaming flag.
+    retriever_name, retriever = select_component(
+        "retriever",
+        retriever_map,
+        chat_args
+    )
 
-    :return: A chain
+    llm_name, llm = select_component("llm", llm_map, chat_args)
+    memory_name, memory = select_component("memory", memory_map, chat_args)
 
-    Example Usage:
+    set_conversation_components(
+        chat_args.conversation_id,
+        llm=llm_name,
+        retriever=retriever_name,
+        memory=memory_name
+    )
 
-        chain = build_chat(chat_args)
-    """
+    condense_question_llm = ChatOpenAI(streaming=False)
 
-    pass
+    return StreamingConversationalRetrievalChain.from_llm(
+        llm=llm,
+        memory=memory,
+        retriever=retriever,
+        condense_question_llm=condense_question_llm,
+        metadata=chat_args.metadata
+    )
---
+++ app/chat/llms/chatopenai.py
@@ -0,0 +1,8 @@
+from langchain.chat_models import ChatOpenAI
+from app.chat.models import ChatArgs
+
+def build_llm(chat_args: ChatArgs, model_name: str):
+  return ChatOpenAI(
+    streaming=chat_args.streaming,
+    model_name=model_name
+  )
